d7b04b05ef19e58f2cb873ae97debeb0d6dea9e5
Enzo-Radel <enzo@radel.com.br>
2022-02-20 19:01:59 -0300

    Segunda tela recebendo os dados do aluno

--- app/src/main/java/com/example/situacaoaluno/ResultActivity.java
+++ app/src/main/java/com/example/situacaoaluno/ResultActivity.java
@@ -2,13 +2,30 @@ package com.example.situacaoaluno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private String name;
+    private double grade1;
+    private double grade2;
+    private int frequency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent it = getIntent();
+        if (it == null) return;
+
+        Bundle params = it.getExtras();
+        if (params == null) return;
+
+        this.name = params.getString("name", "");
+        this.grade1 = params.getDouble("grade1", -1);
+        this.grade2 = params.getDouble("grade2", -1);
+        this.frequency = params.getInt("frequency", -1);
     }
 }
